68fbbea44101de46243c9072b2d97a698bb80d9d
A7mad3bd <ahmadabd398@gmail.com>
2022-03-28 02:14:08 +0300

    stack-queue-brackets

---
+++ stack-and-queue/app/src/test/java/com/pkg/validate_brackets_test.java
@@ -0,0 +1,26 @@
+package com.pkg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class validate_brackets_test {
+
+    @Test
+    public void validateBracketsTest(){
+
+        brackets test = new brackets();
+        assertEquals(true,true);
+        assertEquals(test.validate_brackets("{}"),true);
+        assertEquals(test.validate_brackets("{}(){}"),true);
+        assertEquals(test.validate_brackets("()[[fvdvgd]]"),true);
+        assertEquals(test.validate_brackets("(){}[[]]"),true);
+        assertEquals(test.validate_brackets("{}{Code}[Fellows](())"),true);
+
+        assertEquals(test.validate_brackets("[({}]"),false);
+        assertEquals(test.validate_brackets("(]("),false);
+        assertEquals(test.validate_brackets("{(})"),false);
+
+
+    }
+}
